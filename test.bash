b47be15257a60ad8436b3dd0eced729bd34dbe81
Rene Milk <rene.milk@wwu.de>
2017-08-24 16:06:46 +0200

    back to 500MB testfile

--- test.bash
+++ test.bash
@@ -2,6 +2,6 @@
 
 fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 \
     --name=test --filename=test --bs=4k --iodepth=64 \
-    --size=700M --readwrite=randrw --rwmixread=75 \
+    --size=500M --readwrite=randrw --rwmixread=75 \
     --eta=always  --eta-newline=30 \
     --status-interval=540
