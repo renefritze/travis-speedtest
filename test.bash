09b4c247b85991143d35d9086c1610f5db8eb1f9
Rene Milk <rene.milk@wwu.de>
2017-08-24 14:24:21 +0200

    add travis wait and decrease fio test size

--- test.bash
+++ test.bash
@@ -2,4 +2,4 @@
 
 fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 \
     --name=test --filename=test --bs=4k --iodepth=64 \
-    --size=4G --readwrite=randrw --rwmixread=75
+    --size=2G --readwrite=randrw --rwmixread=75
