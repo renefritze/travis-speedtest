176340deaf3b65972d270934a3dfcfab354ce0b7
Rene Milk <rene.milk@wwu.de>
2017-08-24 11:38:50 +0200

    move sudo to outer run

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: docker docker_push
 
 docker:
-	docker build -t renemilk/travis-speedtest debian_docker
+	docker build -t renemilk/travis-speedtest docker
 
 docker_push: docker
 	docker push renemilk/travis-speedtest
--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CLEAR="sudo /sbin/sysctl -w vm.drop_caches=3"
+CLEAR="/sbin/sysctl -w vm.drop_caches=3"
 
 ${CLEAR}
 dd if=/dev/zero of=/tmp/testfile bs=1G count=1 oflag=direct
@@ -8,4 +8,4 @@ ${CLEAR}
 dd if=/dev/zero of=/tmp/testfile bs=512 count=1000 oflag=direct
 
 ${CLEAR}
-sudo hdparm -Tt /dev/sda
+hdparm -Tt /dev/sda
